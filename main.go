cbdfb9e0963a76bb10a5b57c555140dba0e12b4e
0xlvl3 <cryptochoccy@gmail.com>
2023-02-22 17:11:03 +1100

    Added more to Time conversions.

--- main.go
+++ main.go
@@ -11,26 +11,29 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	
 	// randChoice will influence which question we get.
-	randChoice := rand.Intn(3) + 1 // + 1 so we start at index of 1.
+	randChoice := rand.Intn(1) + 6 // + 1 so we start at index of 1.
 	fmt.Println(randChoice)
 	
 	// Users score.
 	var score int = 0
 	fmt.Println(score)
 
+	// Helper will be added onto all conversions so they aren't 0. 
+	helper := rand.Intn(120) + 1
 
 	switch randChoice {
 	// Time questions
 	case 1:
 		// Years => Days
 
-		// Generate a random number of years; y = years.
+		// Generate a random number of years; y = Years.
 		y := rand.Intn(100) + 1
 
 		// Ask user to convert years to days.
 		fmt.Printf("Convert %v years to days: ", y)
 
-		var d int // d = days.
+		// Get user input; d = Days.
+		var d int
 		fmt.Scanln(&d)
 		
 		// Check equivalence; a = answer.
@@ -47,8 +50,8 @@ func main() {
 	case 2:
 		// Days => Years
 
-		// Generate a random number of days; d = days.
-		d := rand.Intn(3650) + 366
+		// Generate a random number of days; d = Days.
+		d := rand.Intn(3650) + helper
 
 		// Ask user to convert days to years.
 		fmt.Printf("Convert %d days to years, two decimal places: ",d)
@@ -73,53 +76,46 @@ func main() {
 			fmt.Println("Score is", score, ":D")
 		}	
 	case 3:
-		// Hours => Days
+		// Hours => Days 
+		// Hours / 24 = Days.
 		
-		// Generate random amount of days; h = hours.
-		h := rand.Intn(100000) + 100
+		// Generate random amount of days; h = Hours.
+		h := rand.Intn(100000) + helper
 		fmt.Println("Answer format is integer value rounded to 1 decimal place. e.g. 103 ✓, 102.86 ✗")
 		fmt.Printf("How many days are in %v hours: ", h)
 
-		// User input to compare; h = hours.
+		// User input to compare; d = Days.
 		var d int
 		fmt.Scanln(&d)
 
-		// Check equivalence; a = answer
+		// Check equivalence; a = Answer.
 		a := float64(h) / 24 
-		fmt.Println(a)
 		a = math.Round(a*10)/10 // Round to 1 decimal place
-		fmt.Println(a)
 		answer := int(a)
-		fmt.Println(answer)
 
-		// if block to check user input == d
+		// Check user input == answer.
 	    if d == answer{
 			fmt.Printf("Correct %v hours are in %v days", h, answer)
 		} else {
 			fmt.Println("Incorrect")
 		}	
 
-	// todo: finish 4
 	case 4:
-		// Hours => Days
-		// h * d
+		// Days => Hours
+		// Hours * 24 = Days
 		
-		// Generate random amount of hours; h = hours.
-		h := rand.Intn(100000) + 100
-		fmt.Println("Answer format is integer value rounded to 1 decimal place. e.g. 103 ✓, 102.86 ✗")
-		fmt.Printf("How many days are in %v hours: ", h)
+		// Generate random amount of hours; d = days.
+		d := rand.Intn(100000) + helper 
+		fmt.Printf("How many hours are in %v days: ", d)
 
 		// User input to compare; h = hours.
-		var d int
-		fmt.Scanln(&d)
+		var h int
+		fmt.Scanln(&h)
 
 		// Check equivalence; a = answer
-		a := float64(h) / 24
-		a = math.Round(a*10)/10 // Round to 1 decimal place
-		answer := int(a)
+		a := d * 24
 
-		// if block to check user input == d
-	    if h == answer{
+	    if h == a{
 			fmt.Println("Correct")
 		} else {
 			fmt.Println("Incorrect")
@@ -128,12 +124,55 @@ func main() {
 
 	case 5:
 		// Hours => Minutes
+		// 1 => 60
+
+		// Generate random hour; h = Hour
+		h := rand.Intn(3750)+ helper 
+		fmt.Printf("How many minutes are in %v hours: ",h)
+
+		// Get user answer; m = Minute
+		var m int
+		fmt.Scanln(&m)
+
+		// a = Answer.
+		a := h * 60
+
+		// Compare answer == user input.
+		if a == m{
+			fmt.Println("Correct")	
+		} else {
+			fmt.Println("Incorrect")
+		}
+	
 
 	case 6:
 		// Minutes => Hours
+		// 60 Mins => 1 Hour
+
+		// Generate random minute; m = Minute.
+		m := rand.Intn(100000)+ helper 
+		fmt.Printf("How many hours are in %v minutes: ", m)
+
+		// Get user input for hour; h = Hours.
+		var h float64
+		fmt.Scanln(&h)
+		h = math.Round(h*100) / 100 // Round to two decimal places 
+	
+		// Find answer; a = answer.
+		a := float64(m) / 60
+		a = float64(a)
+
+		// Compare answer to user input.
+		if math.Abs(a-h) < 0.01{
+			fmt.Println("Correct!")
+		} else {
+			fmt.Println("Incorrect")
+		}
 
 	case 7:
 		// Seconds => Minutes
+		// 60 seconds = 1 minute.
+	
 
 	case 8:
 		// Minutes => Seconds
